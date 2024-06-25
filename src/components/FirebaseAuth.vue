b46be40bffb9066c472820dc4a818908d09f7a87
kotobukid <kotobuki@daqua.jp>
2024-06-25 15:32:57 +0900

    認証処理（非同期）の完了を待つ

--- src/components/FirebaseAuth.vue
+++ src/components/FirebaseAuth.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
-import {inject, ref, type Ref} from "vue";
-import {getAuth, GoogleAuthProvider, signInWithPopup, type User} from "firebase/auth";
+import {inject, ref, onMounted, type Ref} from "vue";
+import {getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, type User} from "firebase/auth";
 import UserDetail from "@/components/UserDetail.vue";
 
-const authenticatedUser: Ref<User> = ref(null);
-
-const _auth = getAuth();
-authenticatedUser.value = _auth.currentUser;
-
 const auth = inject('auth');
+
+const authenticatedUser: Ref<User> = ref(null);
+const show_detail = ref(false);
+
+onMounted(() => {
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      show_detail.value = true;
+      authenticatedUser.value = user;
+    }
+  });
+});
 
 const signInWithGoogle = async () => {
   try {
@@ -28,16 +35,19 @@ const signInWithGoogle = async () => {
 
 <template>
   <div class="firebaseAuth">
-    <h1>this is firebase auth component</h1>
-    <div class="already_authenticated" v-if="authenticatedUser">
-      <span class="message">you are authenticated.</span>
-      <user-detail :user="authenticatedUser"></user-detail>
+    <h1>Firebase Authentication</h1>
+    <div v-if="show_detail">
+      <div class="already_authenticated" v-if="authenticatedUser">
+        <span class="message">you are authenticated.</span>
+        <user-detail :user="authenticatedUser"></user-detail>
+      </div>
+      <div class="login_required.not" v-else>
+        <span class="message not">You are not authenticated.</span>
+        <br>
+        <button @click="signInWithGoogle">Login with Google</button>
+      </div>
     </div>
-    <div class="login_required.not" v-else>
-      <span class="message">You are not authenticated.</span>
-      <br>
-      <button @click="signInWithGoogle">Login with Google</button>
-    </div>
+    <div v-else>認証処理中(非同期)</div>
   </div>
 </template>
 
@@ -48,13 +58,18 @@ const signInWithGoogle = async () => {
 }
 
 span.message {
+  padding: 5px;
+  display: inline-block;
+  font-size: 1rem;
+  line-height: 1rem;
+
   text-decoration: underline;
   color: green;
   background-color: lightblue;
 
   &.not {
-    color: red;
-    background-color: brown;
+    color: black;
+    background-color: #fdbebe;
   }
 }
 </style>
